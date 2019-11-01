df4865d4e7085822b09547528c49625ec445ed79
Eugene <malenkayaelfiyka@yandex.ru>
2019-11-01 13:37:53 +0300

    Auto stash before merge of "develop" and "master"

---
+++ app/src/main/java/com/example/homework/FirstAnimation.java
@@ -0,0 +1,20 @@
+package com.example.homework;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.View;
+
+public class FirstAnimation extends View {
+
+    public FirstAnimation(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+
+    }
+}
