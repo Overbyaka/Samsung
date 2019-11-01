0e828232987638c13498baffa9f7f4573558b78f
Eugene <malenkayaelfiyka@yandex.ru>
2019-11-01 14:01:49 +0300

    1.8.7 Simple animation
    Был создан класс FirstAnimation. В комментах были проверены, как работают команды. На экране телефона "улетают" шарики. Изменен MainActivity

--- app/src/main/java/com/example/homework/FirstAnimation.java
+++ app/src/main/java/com/example/homework/FirstAnimation.java
@@ -7,14 +7,54 @@ import android.view.View;
 
 public class FirstAnimation extends View {
 
+    int N = 10;
+    float[]x = new float[N];
+    float[]y = new float[N];
+    float[]vx = new float[N];
+    float[]vy = new float[N];
+
     public FirstAnimation(Context context) {
         super(context);
+        /*for(int i = 0; i < N;i++){
+            x[i] = (float)(Math.random() * 500);
+            y[i] = (float)(Math.random() * 500);
+            vx[i] = (float)(Math.random() * 6 -3);
+            vy[i] = (float)(Math.random() * 6 - 3);
+        }*/
     }
+
+    Paint paint = new Paint();
+
+    //float x = 0;
+    //long lastTime = System.currentTimeMillis();
+
+    boolean isStart = false;
 
     @Override
     protected void onDraw(Canvas canvas){
-        super.onDraw(canvas);
-        Paint paint = new Paint();
+        //canvas.drawCircle(x,300,20,paint);
+        //x+=0.5f;
+        /*long nowTime = System.currentTimeMillis();
+        x += 0.1f * (nowTime-lastTime);
+        lastTime = nowTime;
+        invalidate();*/
+
+        if(!isStart){
+            for(int i = 0; i < N;i++){
+                x[i] = (float)(Math.random() * this.getWidth());
+                y[i] = (float)(Math.random() * this.getHeight());
+                vx[i] = (float)(Math.random() * 6 -3);
+                vy[i] = (float)(Math.random() * 6 - 3);
+            }
+            isStart = true;
+        }
+        for (int i = 0; i < N; i++) {
+            canvas.drawCircle(x[i], y[i], 20, paint);
+        }
+        for (int i = 0; i < N; i++) {
+            x[i] += vx[i];
+            y[i] += vy[i];
+        }invalidate();
 
     }
 }
