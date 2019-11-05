8a0b0084cff6bb0425bd2fa164655c3194348a08
Eugene <malenkayaelfiyka@yandex.ru>
2019-11-05 17:30:34 +0300

    1.8.9 ModelSolarSystem
    Добавлен класс modelSolarSystem. MainActivity вызывает modelSolarSystem. На экране телефона появ-лся круг по-средине, вокруг которого двигаются планеты по эллипсу.

--- app/src/main/java/com/example/homework/MainActivity.java
+++ app/src/main/java/com/example/homework/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new Graphics((this)));
+        setContentView(new modelSolarSystem((this)));
     }
 }
