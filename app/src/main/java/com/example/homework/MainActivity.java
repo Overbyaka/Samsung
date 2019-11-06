5cfeded0125c5f172ef4c8f3d79ae780c5a739d2
Eugene <malenkayaelfiyka@yandex.ru>
2019-11-06 11:11:39 +0300

    1.8.10 Pendulum
    Добавлен класс Pendulum.

--- app/src/main/java/com/example/homework/MainActivity.java
+++ app/src/main/java/com/example/homework/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new modelSolarSystem((this)));
+        setContentView(new Pendulum((this)));
     }
 }
