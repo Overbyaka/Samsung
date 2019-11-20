94ba18ce5f79bf7fc27a6ec6a9c67795eec8a7f3
Eugene <malenkayaelfiyka@yandex.ru>
2019-11-20 16:54:41 +0300

    2.5.2 XML
    Вы увидите прекрасное начало в постижении XML. Логин - Mur, пароль - Meow

--- app/src/main/java/com/example/homework/MainActivity.java
+++ app/src/main/java/com/example/homework/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.homework;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -20,38 +21,22 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"create",Toast.LENGTH_SHORT).show();
     }
 
-    public void solveEquation(View view){
-        //ax+b=c
-        double a = Double.parseDouble(((EditText)findViewById(R.id.coefficient_a)).getText().toString());
-        double b = Double.parseDouble(((EditText)findViewById(R.id.coefficient_b)).getText().toString());
-        double c = Double.parseDouble(((EditText)findViewById(R.id.coefficient_c)).getText().toString());
-        double D = b*b - 4*a*c;
+    final String LOGIN= "Mur";
+    final String PASSWORD = "Meow";
+
+    public void entering(View view){
+        String login = ((EditText)findViewById(R.id.login)).getText().toString();
+        String password = ((EditText)findViewById(R.id.password)).getText().toString();
         TextView result = (TextView) findViewById(R.id.result);
-        if(a==0&&b==0){
-            if(c==0) {
-                result.setText("x - любое число");
-            }
-            else{
-                result.setText("x - не существует");
-            }
-        } else if(a==0){
-            result.setText(String.valueOf(-c/b));
-        } else if(b==0){
-            if(a*c>=0){
-                result.setText(String.valueOf(Math.sqrt(-c/a))+ " и " + String.valueOf(-Math.sqrt(-c/a)));
-            }
-            else{
-                result.setText("x - не существует");
-            }
-        } else if(c==0){
-            result.setText("0 и " + String.valueOf(-b/a));
-        } else if(D>0){
-            result.setText(String.valueOf((-b+Math.sqrt(D))/(2*a)) + " и " + String.valueOf((-b-Math.sqrt(D))/(2*a)));
-        } else if(D==0){
-            result.setText(String.valueOf((-b)/(2*a)));
+        if(LOGIN.equals(login) && PASSWORD.equals(password)){
+            result.setTextColor(Color.GREEN);
+            result.setText("Верно");
         }
-        else{
-            result.setText("x - не существует");
+        else {
+            result.setTextColor(Color.RED);
+            result.setText("Вы ошиблись в логине или пароле");
         }
+        ((EditText) findViewById(R.id.login)).setText("");
+        ((EditText) findViewById(R.id.password)).setText("");
     }
 }
